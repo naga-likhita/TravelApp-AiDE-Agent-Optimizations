a889d37aef21554238fb5afbd9829e32674bacc9
naga-likhita <nagalikhita.c@valuelabs.com>
2025-04-07 16:35:25 +0530

    #3 Fine-tune database connection pool configurations to enhance system stability.

--- TravelApp/Program.cs
+++ TravelApp/Program.cs
@@ -1,21 +1,27 @@
 using Microsoft.EntityFrameworkCore;
 using TravelApp;
 
-TravelDbContext.Instance.Import();
-
 var builder = WebApplication.CreateBuilder(args);
+
+// Register DbContext first
+builder.Services.AddDbContextPool<TravelDbContext>(options =>
+    options.UseSqlite(TravelDbContext.DbPath),
+    poolSize: 128);
+
 // Add caching services
 builder.Services.AddMemoryCache();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<TravelDbContext>(o =>
-{
-    o.UseSqlite(TravelDbContext.DbPath);
-});
-
 var app = builder.Build();
+
+// Initialize database using DI
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<TravelDbContext>();
+    context.Import();
+}
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
